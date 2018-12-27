44919dcd53dfa8b39bdacbb90c198799e288b5b3
Silvia Gazzola <40733962+silviagazzola@users.noreply.github.com>
2018-12-27 17:38:51 +0100

    Update Contents.m
    Description for an updated version of IRtools, where new flexible solvers for 1-norm penalization are incorporated

--- Contents.m
+++ Contents.m
@@ -1,6 +1,6 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % IR Tools
-% April 2018
+% December 2018
 % 
 % This file is part of the IR Tools package and is distributed under the
 % 3-Clause BSD License. A separate license file should be provided as
@@ -8,19 +8,19 @@
 % 
 % Copyright 2018 Silvia Gazzola, University of Bath, Per Christian Hansen,
 % Technical University of Denmark and James G. Nagy, Emory University.
+%
+% The present version is extended by J. Chung, Virginia Tech, and 
+% S. Gazzola, University of Bath
 % 
-% Contact: s.gazzola@bath.ac.uk, pcha@dtu.dk, jnagy@emory.edu
+% Contact: jmchung@vt.edu, s.gazzola@bath.ac.uk
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 % Installating
 %   IRtools_setup       - Set up search paths to IRtools
 %
 % Example scripts:
-%   EXblur_cgls_hybrid  - Example script, speckle deblurring problem
-%   EXdiffusion_rrgmres - Example script, inverse diffusion problem
-%   EXinvinterp2_cgls   - Example script, inverse interpolation problem
-%   EXnmr_cgls_mrnsd    - Example script, 2D NMR relaxometry
-%   EXsparsity          - Example script, deblurring without and with sparsity
+%   Example 1
+%   Example 2
 %
 % Iterative methods:
 %   IRart              - Algebraic Reconstruction Technique
@@ -28,10 +28,12 @@
 %   IRconstr_ls        - Least squares solver with box and energy constraints
 %   IRell1             - Least squares solver with 1-norm penalization term
 %   IRenrich           - Enriched CGLS method
-%   IRfista            - FISTA algorithm for constrained least squares problems
+%   IRfista            - FISTA algorithm for constrained least squares problems 
+%                        and for 1-norm penalization
 %   IRget              - Get options for IR Tools functions
 %   IRhtv              - Least squares solver with heuristic total variation penalization
 %   IRhybrid_fgmres    - Hybrid version of FGMRES for enforcing 1-norm penalization
+%   IRhybrid_flsqr     - Hybrid version of FLSQR for enforcing 1-norm penalization
 %   IRhybrid_gmres     - Hybrid version of GMRES algorithm for square systems
 %   IRhybrid_lsqr      - Hybrid version of LSQR algorithm
 %   IRirn              - Least squares solver with 1-norm penalization term
@@ -46,23 +48,4 @@
 %   OPdiffusion        - The forward computation and its adjoint for PRdiffusion
 %   OPinvinterp2       - The forward computation and its adjoint for PRinvinterp2
 %   OPnmr              - The forward computation and its adjoint for PRnmr
-%
-% Test problems and associated functions:
-%   PRblur             - Generates data for use in image deblurring problems
-%   PRblurdefocus      - Image deblurring problem with a defocus point spread function
-%   PRblurgauss        - Image deblurring problem with a Gaussian point spread function
-%   PRblurmotion       - Image deblurring problem with linear motion blur
-%   PRblurrotation     - Image deblurring problem with rotational motion blur
-%   PRblurshake        - Image deblurring problem with random camera motion (shaking)
-%   PRblurspeckle      - Image deblurring problem with a speckle point spread function
-%   PRdiffusion        - Generates data for use in an inverse diffusion problems
-%   PRget              - Get options for IR Tools test problems
-%   PRinvinterp2       - Generates data for use in 2D inverse interpolation problems
-%   PRnmr              - Generates data for 2D NMR relaxometry problems
-%   PRnoise            - Add noise of a specified distribution and level
-%   PRseismic          - Generates data for seismic travel-time tomography problems
-%   PRset              - Set options for IR Tools test problems
-%   PRshowb            - Show the right-hand side b (the data) in IR Tools
-%   PRshowx            - Show the solution x in IR Tools
-%   PRspherical        - Generates data for spherical means tomography problems
-%   PRtomo             - Generates data for X-ray tomographic reconstruction problems
+%   OPblur_transform   - The forward computation and its adjoint for PRblur, incorporating sparsity transform
